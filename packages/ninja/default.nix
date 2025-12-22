cf6896f520ef86dfe71ef3553a835141fc882f41
Philip Taron <philip.taron@gmail.com>
2025-12-22 15:15:24 -0800

    refactor: use postFetch for ninja to avoid extra derivation
    Use fetchzip's postFetch to move ninja into bin/ directory,
    eliminating the separate runCommand wrapper derivation.

--- packages/ninja/default.nix
+++ packages/ninja/default.nix
@@ -1,11 +1,9 @@
 { pkgs, perSystem, ... }:
-let
-  ninjaSrc = pkgs.fetchzip {
-    inherit (perSystem.self.tools-json.ninja) url hash;
-    stripRoot = false;
-  };
-in pkgs.runCommand "ninja-wrapped" {} ''
-  mkdir -p $out/bin
-  cp ${ninjaSrc}/ninja $out/bin/
-  chmod +x $out/bin/ninja
-''
+pkgs.fetchzip {
+  inherit (perSystem.self.tools-json.ninja) url hash;
+  stripRoot = false;
+  postFetch = ''
+    mkdir -p $out/bin
+    mv $out/ninja $out/bin/
+  '';
+}
